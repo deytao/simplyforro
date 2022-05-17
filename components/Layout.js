4f91b7295f25eaddeef15043f43984c6020a516a
Jonathan Camile <jonathan.camile@protonmail.com>
2022-05-17 18:23:49 +0200

    feat(theme): add logo and favicon

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,10 @@ export default function Layout({ children }) {
     <>
       <h1 className="text-6xl font-bold py-4 text-center">
         <Link href="/">
-            <a>SimplyForró</a>
+            <a><img src="/simplyforro.gif" alt="SimpyForró" title="SimpyForró" className="object-scale-down h-60 inline" /></a>
         </Link>
       </h1>
-      <main className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="flex min-h-screen flex-col items-center py-2">
         {children}
       </main>
     </>
