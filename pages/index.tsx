bdf55b2a8507a7f31a231ede5bca2bbda066d0bc
Jonathan Camile <jonathan.camile@protonmail.com>
2022-05-17 19:02:19 +0200

    feat(school): create new pages to introduce the school

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,41 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <>
-     <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-       <a
-         href="https://simplyforro.notion.site/42f9fe6ead9544338eb4d5ee5c85e13e"
-         className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-       >
-         <h3 className="text-2xl font-bold">Calendar &rarr;</h3>
-         <p className="mt-4 text-xl">
-           Keep yourself up to date with the Forró events in the world.
-         </p>
-       </a>
+    <div className="mt-6 grid grid-cols-2 gap-4 max-w-4xl sm:w-full">
 
-       <Link href="/calendar/form">
-           <a
-             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-           >
-             <h3 className="text-2xl font-bold">Add events &rarr;</h3>
-             <p className="mt-4 text-xl">
-                 Help us inform the community about the next dances.
-             </p>
-           </a>
-       </Link>
+      <Link href="/school">
+        <a
+          className="col-span-2 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+        >
+          <h3 className="text-2xl font-bold">The school &rarr;</h3>
+          <p className="mt-4 text-xl">
+              Understand what
+          </p>
+        </a>
+      </Link>
 
-     </div>
-    </>
+      <a
+        href="https://simplyforro.notion.site/42f9fe6ead9544338eb4d5ee5c85e13e"
+        className="rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      >
+        <h3 className="text-2xl font-bold">Calendar &rarr;</h3>
+        <p className="mt-4 text-xl">
+          Keep yourself up to date with the Forró events in the world.
+        </p>
+      </a>
+
+      <Link href="/calendar/form">
+          <a
+            className="rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Add events &rarr;</h3>
+            <p className="mt-4 text-xl">
+                Help us inform the community about the next dances.
+            </p>
+          </a>
+      </Link>
+
+    </div>
   )
 }
 
---
+++ pages/school/course.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Course: NextPage = () => {
+  return (
+    <div className="mt-6 grid grid-cols-2 gap-4 max-w-4xl sm:w-full">
+    The class
+    </div>
+  )
+}
+
+export default Course
