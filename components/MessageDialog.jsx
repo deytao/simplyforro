d3b1934b6050d7c7764be18fd14fbb13e757da96
Jonathan Camile <jonathan.camile@protonmail.com>
2022-05-25 10:45:44 +0200

    feat(calendar): pimp dialog when submitting event form

--- components/MessageDialog.jsx
+++ components/MessageDialog.jsx
@@ -3,14 +3,18 @@ import { Dialog } from '@headlessui/react'
 
 
 const statusClasses = {
-    "success": 'bg-green-100 border-green-500 text-green-700',
-    "error": 'bg-red-100 border-red-500 text-red-700',
-    "info": 'bg-teal-100 border-teal-500 text-teal-700',
-    "warning": 'bg-orange-100 border-orange-500 text-orange-700'
+    "success": ["bg-green-100 border-green-700 text-green-700", "bg-green-700 hover:bg-green-900"],
+    "error": ["bg-red-100 border-red-700 text-red-700", "bg-red-700 hover:bg-red-900"],
+    "info": ["bg-teal-100 border-teal-700 text-teal-700", "bg-teal-700 hover:bg-teal-900"],
+    "warning": ["bg-orange-100 border-orange-700 text-orange-700", "bg-orange-700 hover:bg-orange-900"]
 }
 
 
 export const MessageDialog = ({ messageDialog, setMessageDialog }) => {
+  if (!(messageDialog.status in statusClasses)) {
+    return <></>
+  }
+  const [ panelClasses, buttonClasses ] = statusClasses[messageDialog.status]
   return (
     <Dialog
       open={messageDialog.isOpen}
@@ -19,14 +23,16 @@ export const MessageDialog = ({ messageDialog, setMessageDialog }) => {
     >
       <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
       <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className={`border-t-4 rounded-b px-4 py-3 shadow-md w-full max-w-sm ${statusClasses[messageDialog.status]}`}>
+        <Dialog.Panel className={`border-t-4 rounded-b px-4 py-3 shadow-md w-full max-w-sm ${panelClasses}`}>
           <Dialog.Title className="font-bold">{messageDialog.title}</Dialog.Title>
 
-          <Dialog.Description className="text-sm">
+          <Dialog.Description className="text-sm my-5">
             {messageDialog.message}
           </Dialog.Description>
 
-          <button onClick={() => setMessageDialog({isOpen: false})}>Close</button>
+          <button onClick={() => setMessageDialog({isOpen: false})} class={`${buttonClasses} text-white font-bold py-2 px-4 rounded`}>
+            Close
+          </button>
         </Dialog.Panel>
       </div>
     </Dialog>
