6c8cd76574ecebe06fc3f65e8ac16e9f6660c693
Jonathan Camile <jonathan.camile@protonmail.com>
2022-10-21 15:09:56 +0200

    feat(subscriptions): Create models

---
+++ prisma/migrations/20221021130751_user_subscriptions/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "subscription" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "next_run" TIMESTAMP NOT NULL,
+    "last_run" TIMESTAMP,
+    "frequency" "Frequency" NOT NULL,
+
+    CONSTRAINT "subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subscriber" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "subscription_id" INTEGER NOT NULL,
+
+    CONSTRAINT "subscriber_pkey" PRIMARY KEY ("id")
+);
+
+-- First Subscriptions
+INSERT INTO subscription (updated_at, title, description, slug, active, next_run, frequency) VALUES
+    (CURRENT_TIMESTAMP, 'Next Events', 'Upcoming events for the following week', 'next-events-weekly', false, NOW(), 'weekly'::"Frequency"),
+    (CURRENT_TIMESTAMP, 'Last Updated Events', 'Recently updated events', 'last-updated-events-weekly', false, NOW(), 'weekly'::"Frequency"),
+    (CURRENT_TIMESTAMP, 'Events To Review', 'Events still pending review', 'events-to-review-daily', false, NOW(), 'daily'::"Frequency")
+;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscription_slug_key" ON "subscription"("slug");
+
+
+-- AddForeignKey
+ALTER TABLE "subscriber" ADD CONSTRAINT "subscriber_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscriber" ADD CONSTRAINT "subscriber_subscription_id_fkey" FOREIGN KEY ("subscription_id") REFERENCES "subscription"("id") ON DELETE CASCADE ON UPDATE CASCADE;
