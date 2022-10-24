e5b292dc1d978ceb47c73caedad0df3451df9e1b
Jonathan Camile <jonathan.camile@protonmail.com>
2022-10-24 10:37:44 +0200

    feat(subscriptions): Retrieve message to send and recipients

---
+++ lib/subscription.ts
@@ -0,0 +1,33 @@
+import moment from 'moment';
+import { Subscription, Frequency } from '@prisma/client';
+
+import prisma from 'lib/prisma';
+
+
+export async function GetNextSubscriptions(): Promise<Subscription[]> {
+    let subscriptions: Subscription[] = []
+    const date = moment().add({'hours': 3}).toDate()
+    try {
+        subscriptions = await prisma.subscription.findMany({
+            where: {
+                active: {
+                    equals: true,
+                },
+                nextRun: {
+                    lte: date,
+                },
+            },
+            include: {
+                subscribers: {
+                    include: {
+                        user: true,
+                    },
+                },
+            },
+        })
+    }
+    catch (e) {
+        console.error(e)
+    }
+    return subscriptions
+}
--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,7 +1,8 @@
 import moment from 'moment';
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Category, Event, Role, ValidationStatus } from '@prisma/client';
+import { Category, Event, Role, Subscription } from '@prisma/client';
 
+import { sendBulkEmails } from 'lib/mailjet';
 import { GetNextSubscriptions } from 'lib/subscription';
 
 export default async function handler(
@@ -17,8 +18,13 @@ export default async function handler(
     try {
         if (token === CRON_TOKEN) {
             // Process the POST request
-            res.status(200).json({ success: 'true' })
-        } else {
+            const subscriptions: Subscription[] = await GetNextSubscriptions()
+            const recipients = subscriptions.map((subscription: Subscription) => {
+                return subscription.subscribers.map((subscriber) => subscriber.user.email)
+            }).flat()
+            res.status(200).json(subscriptions)
+        }
+        else {
             res.status(401)
         }
     } catch(err) {
