4bbeb947a273d572e60a64b06324c8b332228405
Jonathan Camile <jonathan.camile@protonmail.com>
2022-12-07 16:30:19 +0100

    feat(flowbite_ui): Add Footer and react-icons

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,6 @@
+import { Footer } from "flowbite-react";
+import moment from "moment";
+import { BsFacebook, BsInstagram } from 'react-icons/bs';
 import { Navbar } from "components/Navbar";
 
 export default function Layout({ children }) {
@@ -5,6 +8,19 @@ export default function Layout({ children }) {
         <>
             <Navbar />
             <main className="flex min-h-screen flex-col items-center">{children}</main>
+            <Footer container={true} className="flex justify-between">
+                <Footer.Copyright href="/" by="SimplyForró" year={moment().format("YYYY")} />
+                <div className="flex justify-center gap-2">
+                    <Footer.Icon
+                      href="https://www.facebook.com/groups/401383903898367/"
+                      icon={BsFacebook}
+                    />
+                    <Footer.Icon
+                      href="https://www.instagram.com/simplyforro/"
+                      icon={BsInstagram}
+                    />
+                </div>
+            </Footer>
         </>
     );
 }
