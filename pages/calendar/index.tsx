06173851b46b474b729051d56a31c0e7f7132d8d
Jonathan Camile <jonathan.camile@protonmail.com>
2022-12-16 16:57:14 +0100

    feat(event_flyer): Add zoom for event image

--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -12,7 +12,7 @@ import { useForm } from "react-hook-form";
 import { LEFT, RIGHT, SwipeEventData, useSwipeable } from "react-swipeable";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
-import { IoLocationOutline } from "react-icons/io5";
+import { FiZoomIn } from "react-icons/fi";
 import {
     HiArrowTopRightOnSquare,
     HiCalendar,
@@ -22,6 +22,7 @@ import {
     HiPlus,
     HiRss,
 } from "react-icons/hi2";
+import { IoLocationOutline } from "react-icons/io5";
 
 import { EventDetailsSimple } from "components/EventPreview";
 import { IModal, Modal } from "components/Modal";
@@ -433,6 +434,7 @@ const Calendar: NextPage<Props> = ({ subscriptions }) => {
                         setCurrentDate(newDate);
                     }}
                     onSelectEvent={(event: Event) => {
+                        const defaultImgClasses = ["h-48", "justify-self-center"]
                         let state: IModal = {
                             isOpen: true,
                             status: "neutral",
@@ -440,12 +442,32 @@ const Calendar: NextPage<Props> = ({ subscriptions }) => {
                             message: (
                                 <div className="flex items-center justify-around gap-2">
                                     {event.imageDataUrl && (
-                                        <div className="basis-1/2 grid bg-slate-300 dark:bg-gray-600 rounded-lg overflow-hidden">
+                                        <div
+                                            className="relative basis-1/2 hover:cursor-zoom-in grid bg-slate-300 dark:bg-gray-600 rounded-lg overflow-hidden"
+                                            onClick={(e) => {
+                                                const zoomImgClasses = ["fixed", "left-0", "top-0", "w-screen", "h-screen", "bg-black", "hover:cursor-zoom-out"]
+                                                const img = document.getElementById(`event-img-${event.id}`)!;
+                                                const classes = img.classList;
+                                                if (img.dataset.isZoomed === "yes") {
+                                                    classes.remove(...zoomImgClasses)
+                                                    classes.add(...defaultImgClasses)
+                                                    img.dataset.isZoomed = "no"
+                                                }
+                                                else {
+                                                    classes.remove(...defaultImgClasses)
+                                                    classes.add(...zoomImgClasses)
+                                                    img.dataset.isZoomed = "yes"
+                                                }
+                                            }}
+                                        >
                                             <img
+                                                id={`event-img-${event.id}`}
                                                 src={event.imageDataUrl}
                                                 alt={event.title}
-                                                className="h-48 justify-self-center object-contain"
+                                                className={`object-contain ${defaultImgClasses.join(" ")}`}
+                                                data-is-zoomed="no"
                                             />
+                                            <FiZoomIn className="h-7 w-7 text-white opacity-50 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
                                         </div>
                                     )}
                                     <div className="basis-1/2 grow flex flex-col gap-1 text-sm md:text-base text-black dark:text-white">
